13a980fb94b5df13b6d9f31fbaba18baac1e124f
과제2.c <usuhuu420@gmail.com>
2024-06-14 21:05:21 +0900

    frontend Push

--- index.js
+++ index.js
@@ -2,6 +2,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
- 
 console.log(test);
 AppRegistry.registerComponent(appName, () => App);
